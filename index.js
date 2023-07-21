16840e4a2632eb54e23186d45f5af74f28dac95f
Devin Anga <DevinAnga.dev@Gmail.com>
2023-07-20 20:50:19 -0400

    added foundational files

---
+++ index.js
@@ -0,0 +1,24 @@
+const pokemonData = {
+    name: "Bulbasaur",
+    id: "0001",
+    image: "N/A",
+    description: "There is a plant seed on its back right from the day this Pokemon is born.  The seed slowly grows larger.",
+    weaknesses: [
+      "Fire",
+      "Psychic",
+      "Ice"
+    ],
+    type: [
+      "Grass",
+      "Poison"
+    ],
+    stats: {
+      height: "2' 04",
+      category: "Seed",
+      weight: "15.2 lbs",
+      gender: "male | female",
+      abilities: [
+        "Overgrow"
+      ]
+    }
+  }
