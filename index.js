3a00786d0a2a2b8d2ab8069f711510fe4717f5e2
Devin Anga <DevinAnga.dev@Gmail.com>
2023-08-09 17:51:15 -0400

    built updated header for mobile/desktop

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const pokemonData = {
-    name: "Bulbasaur",
-    id: "0001",
-    image: "N/A",
-    description: "There is a plant seed on its back right from the day this Pokemon is born.  The seed slowly grows larger.",
-    weaknesses: [
-      "Fire",
-      "Psychic",
-      "Ice"
-    ],
-    type: [
-      "Grass",
-      "Poison"
-    ],
-    stats: {
-      height: "2' 04",
-      category: "Seed",
-      weight: "15.2 lbs",
-      gender: "male | female",
-      abilities: [
-        "Overgrow"
-      ]
-    }
-}   
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  var y = document.getElementById("search-header");
+  var z = document.getElementById("main");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "block";
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+  }
+}
